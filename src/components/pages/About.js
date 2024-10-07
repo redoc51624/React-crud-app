34cc3c14e492d9b02916764a67bfbb1eae3181f7
vikas kumar <vikas10145.12@bitmesra.ac.in>
2024-10-07 20:11:36 +0530

    content changes
    -content changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,19 +6,18 @@ const About = () => {
             <div className='py-4'>
                 <h1>About page</h1>
                 <p className='lead'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
-                Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
-                unknown printer took a galley of type and scrambled it to make a type specimen book.
+                CRUD operations are a set of four functions that allow users to create, read, update, and delete data in a React application: 
+                Create: Allows users to create new entries 
+
                 </p>
                 <p className='lead'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
-                Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
-                unknown printer took a galley of type and scrambled it to make a type specimen book.
+                Read: Allows users to retrieve existing data based on search queries 
+                Update: Allows users to modify existing data 
                 </p>
                 <p className='lead'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
-                Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
-                unknown printer took a galley of type and scrambled it to make a type specimen book.
+                Delete: Allows users to remove entries that are no longer needed 
+                How to Do Crud Operations in ReactJS? - Scaler Topics
+                CRUD stands for Create, Read, Update, and Delete. These operations are essential for dynamic applications that interact with databases or other forms of data storage
                 </p>
             </div>
         </div>
